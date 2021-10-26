e31ca6525bd2c4e8245fd71467356509c661cc5a
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-10-26 16:17:57 +0300

    Writer Add Blog

---
+++ BusinessLayer/ValidationRules/BlogValidator.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class BlogValidator : AbstractValidator<Blog>
+    {
+        public BlogValidator()
+        {
+            RuleFor(x => x.BlogTitle).NotEmpty().WithMessage("Blog Başlığını Boş Bırakmayınız...");
+            RuleFor(x => x.BlogContent).NotEmpty().WithMessage("Blog İçeriğini Boş Bırakmayınız...");
+            RuleFor(x => x.BlogImage).NotEmpty().WithMessage("Blog Görselini Boş Bırakmayınız...");
+            RuleFor(x => x.BlogTitle).MaximumLength(150).WithMessage("Lütfen 150 karakterden az veri girişi yapınız");
+            RuleFor(x => x.BlogTitle).MinimumLength(10).WithMessage("Lütfen 10 karakterden fazla veri girişi yapınız");
+        }
+    }
+}
--- CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/Controllers/BlogController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,7 +16,7 @@ namespace CoreDemo.Controllers
     public class BlogController : Controller
     {
         BlogManager bm = new BlogManager(new EfBlogRepository());
-       
+
         public IActionResult Index()
         {
             var values = bm.GetBlogListWithCategory();
@@ -27,9 +30,36 @@ namespace CoreDemo.Controllers
         }
         public IActionResult BlogListByWriter()
         {
-         var values= bm.GetListByWriter(1);
+            var values = bm.GetListByWriter(1);
             return View(values);
         }
-       
+        [HttpGet]
+        public IActionResult BlogAdd()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogAdd(Blog p)
+        {
+            BlogValidator bv = new BlogValidator();
+            ValidationResult results = bv.Validate(p);
+            if (results.IsValid)
+            {
+                p.BlogStatus = true;
+                p.BlogCreateDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+                p.WriterId = 1;
+                bm.TAdd(p);
+                return RedirectToAction("BlogListByWriter", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
     }
 }
