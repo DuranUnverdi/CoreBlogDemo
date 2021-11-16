67ea83878774965e5c8691bd9c5d9df818e8e6a4
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-11-16 14:49:09 +0300

    dashboard statistic added

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,10 +12,14 @@ namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         [AllowAnonymous]
         public IActionResult Index()
         {
-
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count().ToString();
+            ViewBag.v2 = c.Blogs.Where(x => x.WriterId == 1).Count();
+            ViewBag.v3 = c.Categories.Count();
             return View();
         }
     }
