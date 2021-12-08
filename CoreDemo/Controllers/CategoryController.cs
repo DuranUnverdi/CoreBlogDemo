253a1f67bc76ce94f2558702cdf8093b30faf86f
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-12-08 18:19:53 +0300

    Pagination use

--- CoreDemo/Controllers/CategoryController.cs
+++ CoreDemo/Controllers/CategoryController.cs
@@ -5,15 +5,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace CoreDemo.Controllers
 {
     public class CategoryController : Controller
     {
         CategoryManager cm =new CategoryManager(new EfCategoryRepository()); 
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = cm.GetList();
+            var values = cm.GetList().ToPagedList(page,3);
             return View(values);
         }
     }
