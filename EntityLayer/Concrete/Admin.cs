9155abf2ba1131657b92c66da124c97ac1a66a57
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2021-12-14 19:01:50 +0300

    Admin Table Added

---
+++ EntityLayer/Concrete/Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EntityLayer.Concrete
+{
+   public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public string Role { get; set; }
+
+    }
+}
