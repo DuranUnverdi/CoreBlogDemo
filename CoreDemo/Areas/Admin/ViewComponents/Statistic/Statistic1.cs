734c2173f58f59e4f392b6a1f127cbe8b834c565
DuranUnverdi <75857104+DuranUnverdi@users.noreply.github.com>
2022-01-04 17:27:30 +0300

    api with weather get proc

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -19,6 +20,10 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = cm.GetList().Count();
             ViewBag.v3 = c.Comments.Count();
+            string api = "51caffad34dd9576e39a0051ad635620";
+            string conn = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid="+api;
+            XDocument document = XDocument.Load(conn);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
